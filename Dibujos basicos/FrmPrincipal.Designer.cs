1c9365e669a77e2fdbc384e58b88559c6ce2e1b2
Bryan Enrique Gonzalez <gonzalezzbryan220@gmail.com>
2020-12-01 15:25:03 -0700

    Interfaz grafica OK

---
+++ Dibujos basicos/FrmPrincipal.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Dibujos_basicos
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbZonaDibujo = new System.Windows.Forms.PictureBox();
+            this.btnLinea = new System.Windows.Forms.Button();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnElipse = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnCuadrado = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnTriangulo = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCirculo = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbZonaDibujo)).BeginInit();
+            this.pnlContenedor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbZonaDibujo
+            // 
+            this.pbZonaDibujo.Location = new System.Drawing.Point(27, 38);
+            this.pbZonaDibujo.Name = "pbZonaDibujo";
+            this.pbZonaDibujo.Size = new System.Drawing.Size(329, 362);
+            this.pbZonaDibujo.TabIndex = 0;
+            this.pbZonaDibujo.TabStop = false;
+            // 
+            // btnLinea
+            // 
+            this.btnLinea.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLinea.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLinea.FlatAppearance.BorderSize = 0;
+            this.btnLinea.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnLinea.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btnLinea.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLinea.Font = new System.Drawing.Font("Swis721 BlkCn BT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinea.Location = new System.Drawing.Point(0, 0);
+            this.btnLinea.Name = "btnLinea";
+            this.btnLinea.Size = new System.Drawing.Size(226, 29);
+            this.btnLinea.TabIndex = 1;
+            this.btnLinea.Text = "LINEA";
+            this.btnLinea.UseVisualStyleBackColor = true;
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.Controls.Add(this.panel5);
+            this.pnlContenedor.Controls.Add(this.btnElipse);
+            this.pnlContenedor.Controls.Add(this.panel4);
+            this.pnlContenedor.Controls.Add(this.btnCuadrado);
+            this.pnlContenedor.Controls.Add(this.panel3);
+            this.pnlContenedor.Controls.Add(this.btnTriangulo);
+            this.pnlContenedor.Controls.Add(this.panel2);
+            this.pnlContenedor.Controls.Add(this.btnCirculo);
+            this.pnlContenedor.Controls.Add(this.panel1);
+            this.pnlContenedor.Controls.Add(this.btnLinea);
+            this.pnlContenedor.Location = new System.Drawing.Point(376, 66);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(226, 190);
+            this.pnlContenedor.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(188)))), ((int)(((byte)(242)))));
+            this.panel5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 165);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(226, 5);
+            this.panel5.TabIndex = 10;
+            // 
+            // btnElipse
+            // 
+            this.btnElipse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnElipse.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnElipse.FlatAppearance.BorderSize = 0;
+            this.btnElipse.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnElipse.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btnElipse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnElipse.Font = new System.Drawing.Font("Swis721 BlkCn BT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnElipse.Location = new System.Drawing.Point(0, 136);
+            this.btnElipse.Name = "btnElipse";
+            this.btnElipse.Size = new System.Drawing.Size(226, 29);
+            this.btnElipse.TabIndex = 9;
+            this.btnElipse.Text = "ELIPSE";
+            this.btnElipse.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(188)))), ((int)(((byte)(242)))));
+            this.panel4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 131);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(226, 5);
+            this.panel4.TabIndex = 8;
+            // 
+            // btnCuadrado
+            // 
+            this.btnCuadrado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCuadrado.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCuadrado.FlatAppearance.BorderSize = 0;
+            this.btnCuadrado.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCuadrado.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btnCuadrado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCuadrado.Font = new System.Drawing.Font("Swis721 BlkCn BT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCuadrado.Location = new System.Drawing.Point(0, 102);
+            this.btnCuadrado.Name = "btnCuadrado";
+            this.btnCuadrado.Size = new System.Drawing.Size(226, 29);
+            this.btnCuadrado.TabIndex = 7;
+            this.btnCuadrado.Text = "CUADRADO";
+            this.btnCuadrado.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(188)))), ((int)(((byte)(242)))));
+            this.panel3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 97);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(226, 5);
+            this.panel3.TabIndex = 6;
+            // 
+            // btnTriangulo
+            // 
+            this.btnTriangulo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTriangulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTriangulo.FlatAppearance.BorderSize = 0;
+            this.btnTriangulo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnTriangulo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btnTriangulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTriangulo.Font = new System.Drawing.Font("Swis721 BlkCn BT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTriangulo.Location = new System.Drawing.Point(0, 68);
+            this.btnTriangulo.Name = "btnTriangulo";
+            this.btnTriangulo.Size = new System.Drawing.Size(226, 29);
+            this.btnTriangulo.TabIndex = 5;
+            this.btnTriangulo.Text = "TRIANGULO";
+            this.btnTriangulo.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(188)))), ((int)(((byte)(242)))));
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(226, 5);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnCirculo
+            // 
+            this.btnCirculo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCirculo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCirculo.FlatAppearance.BorderSize = 0;
+            this.btnCirculo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCirculo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btnCirculo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCirculo.Font = new System.Drawing.Font("Swis721 BlkCn BT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCirculo.Location = new System.Drawing.Point(0, 34);
+            this.btnCirculo.Name = "btnCirculo";
+            this.btnCirculo.Size = new System.Drawing.Size(226, 29);
+            this.btnCirculo.TabIndex = 3;
+            this.btnCirculo.Text = "CIRCULO";
+            this.btnCirculo.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(188)))), ((int)(((byte)(242)))));
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 5);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Swis721 Blk BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(63)))), ((int)(((byte)(95)))));
+            this.label1.Location = new System.Drawing.Point(439, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "DIBUJAR:";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(78)))), ((int)(((byte)(146)))));
+            this.btnLimpiar.FlatAppearance.BorderSize = 2;
+            this.btnLimpiar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.btnLimpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Swis721 Blk BT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(416, 292);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(141, 34);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlContenedor);
+            this.Controls.Add(this.pbZonaDibujo);
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbZonaDibujo)).EndInit();
+            this.pnlContenedor.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbZonaDibujo;
+        private System.Windows.Forms.Button btnLinea;
+        private System.Windows.Forms.Panel pnlContenedor;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnElipse;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnCuadrado;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnTriangulo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCirculo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
